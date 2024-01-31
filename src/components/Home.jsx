990df2243b723b5a3a3ac2fabd1754de84a01e7b
gautam-bhagat <gautambhagat.business@gmail.com>
2024-02-01 01:31:42 +0530

    services gallery clients

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import work from '../assests/work.jpg'
-import oceanbg from '../assests/ocean-bg.jpg'
+import commercialshoot from '../assests/services/commercial-shoot.jpg'
+import cocacola from '../assests/logos/cocacola.svg'
+import mcdonald from '../assests/logos/mcdonald.svg'
+import medium from '../assests/logos/medium.svg'
+import quora from '../assests/logos/quora.svg'
+import whatsapp from '../assests/logos/whatsapp.svg'
 
 function Home() {
     return (
@@ -8,7 +13,7 @@ function Home() {
 
             <div className="flex px-10 z-0" style={{ height: '90vh' }}>
 
-                <img src={work} className='z-0' style={{ width: '60vw' }} />
+                <img src={work} className='z-0' alt='work ' style={{ width: '60vw' }} />
 
                 <div className='flex items-center '>
                     <div className='text-right items-right'>
@@ -22,7 +27,7 @@ function Home() {
 
             </div>
 
-            <section id="about" className="text-white z-10 flex items-center flex-col" style={{ backgroundImage: "url(" + "https://res.cloudinary.com/dmtrqlxe4/image/upload/v1703319646/htsbqvqty1dvmbipdv44.jpg" + ")", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height:'80vh'}} >
+            <section id="about" className="text-white z-10 flex items-center flex-col" style={{ backgroundImage: "url(https://res.cloudinary.com/dmtrqlxe4/image/upload/v1703319646/htsbqvqty1dvmbipdv44.jpg)", backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', height: '80vh' }} >
 
                 <h1 className='font-bold text-3xl text-center p-10'>WHO ARE WE?</h1>
                 <div className='flex items-center justify-center'>
@@ -35,6 +40,117 @@ function Home() {
                 </div>
             </section>
 
+            <div className='w-full  bg-white' style={{ marginTop: '-45px', borderTopLeftRadius: "50px", borderTopRightRadius: "50px" }}>
+
+                <section className='m-5 w-5/6 mx-auto' id='services'>
+                    <h1 className='text-center p-12  text-3xl font-bold' >Services</h1>
+                    <div className='grid grid-cols-3 gap-5'>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img className="w-2/3 blueshadow rounded-full" src={commercialshoot} alt="" srcset="" />
+                            <h2 className='m-2  font-bold text-xl'>Commercial Shoot</h2>
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img className="w-2/3 blueshadow rounded-full" src={`https://picsum.photos/300/300?grayscale`} alt="" srcset="" />
+                            <h2 className='m-2  font-bold text-xl'>Commercial Shoot</h2>
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img className="w-2/3 blueshadow rounded-full" src={commercialshoot} alt="" srcset="" />
+                            <h2 className='m-2  font-bold text-xl'>Commercial Shoot</h2>
+                        </div>
+
+
+                    </div><div className='mt-12 grid grid-cols-2 gap-5'>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img className="w-2/5 blueshadow rounded-full" src={commercialshoot} alt="" srcset="" />
+                            <h2 className='m-2  font-bold text-xl'>Commercial Shoot</h2>
+                        </div><div className='flex justify-center items-center text-center flex-col item'>
+                            <img className="w-2/5 blueshadow rounded-full" src={commercialshoot} alt="" srcset="" />
+                            <h2 className='m-2  font-bold text-xl'>Commercial Shoot</h2>
+                        </div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </section>
+
+                <section className='m-5 w-3/4 mx-auto' id='gallery'>
+                    <h1 className='text-center p-12  text-3xl font-bold' >Gallery</h1>
+                    <div className='grid grid-cols-3 gap-2'>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img src="https://picsum.photos/300/200" className='w-full' alt="" srcset="" />
+
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img src="https://picsum.photos/300/200" alt="" className='w-full' srcset="" />
+
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img src="https://picsum.photos/300/200" alt="" className='w-full' srcset="" />
+
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img src="https://picsum.photos/300/200" className='w-full' alt="" srcset="" />
+
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img src="https://picsum.photos/300/200" alt="" className='w-full' srcset="" />
+
+                        </div>
+                        <div className='flex justify-center items-center text-center flex-col item'>
+                            <img src="https://picsum.photos/300/200" alt="" className='w-full' srcset="" />
+
+                        </div>
+
+
+                    </div>
+                </section>
+
+                <section className='m-12 w-3/4 mx-auto' id='client'>
+                    <h1 className='text-center p-12  text-3xl font-bold' >Clients</h1>
+                    <div class="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+                        <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+                            <li>
+                                <img src={cocacola} alt="CocaCola" />
+                            </li>
+                        
+                            <li>
+                                <img src={mcdonald} alt="MCDonald" />
+                            </li>
+                            <li>
+                                <img src={medium} alt="Medium" />
+                            </li>
+                            <li>
+                                <img src={quora} alt="Quora" />
+                            </li>
+                            <li>
+                                <img src={whatsapp} alt="Whatsapp" />
+                            </li>
+
+                        </ul>
+                        <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+                            <li>
+                                <img src={cocacola} alt="CocaCola" />
+                            </li>
+                        
+                            <li>
+                                <img src={mcdonald} alt="MCDonald" />
+                            </li>
+                            <li>
+                                <img src={medium} alt="Medium" />
+                            </li>
+                            <li>
+                                <img src={quora} alt="Quora" />
+                            </li>
+                            <li>
+                                <img src={whatsapp} alt="Whatsapp" />
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </section>
+            </div>
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import logo from '../assests/OCEAN_STUDIO_LOGO.png'
 
 function Navbar() {
     return (
         <nav className='h-20  font-regular w-full bg-transparent flex  justify-center items-center  text-white'>
 
 
-            <div className='flex pr-10 bg-gradient-to-b  from-richelectricblue to-oxfordblue p-5 shadow-inner shadow-white ' style={{ borderRadius: 50 }}>
-                <div className="logo mr-20">
-                    kj
+            <div className='flex bg-gradient-to-b  items-center py-1 pl-2 from-richelectricblue to-oxfordblue shadow-inner shadow-white ' style={{ borderRadius: 50 }}>
+                <div  className=" logo mr-1">
+                    <img src={logo} className='w-12' alt="react logo" />
+                </div>
+                <div className="logo-heading mr-20">
+                    OCEAN STUDIO
                 </div>
                 <ul className='flex '>
                     <li className='mr-20 '>
